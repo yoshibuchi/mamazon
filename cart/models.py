68e88efa97cd1629d81ef144aab46cdb45417c56
yoshibuchi <yoshibuchi.n@gmail.com>
2020-10-04 21:20:40 +0900

    lesson08

--- cart/models.py
+++ cart/models.py
@@ -5,7 +5,7 @@ from mamazon.models import Product
 User = settings.AUTH_USER_MODEL
 
 class Cart(models.Model):
-    user = modelsForeignKey(User, null = True, blank = True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null = True, blank = True, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
